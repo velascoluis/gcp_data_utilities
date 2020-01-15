d1afed4ab51e5ef8e0a0469850d693cd142e21d3
velascoluis <velascoluis@google.com>
2020-01-15 13:19:10 -0800

    Add support for simple DDL generation

--- BigQuery/bq_crawler.py
+++ BigQuery/bq_crawler.py
@@ -4,14 +4,16 @@ import argparse
 import csv
 
 
-parser = argparse.ArgumentParser(description='Videos to images')
+parser = argparse.ArgumentParser(description='BQ Schema Extractor utils')
 parser.add_argument('project', type=str, help='The project that contains the BigQuery')
-parser.add_argument('csv_path', type=str, help='Output dir for image')
+parser.add_argument('operation_mode', type=str, help='CSV or DDL operation')
+parser.add_argument('file_path', type=str, help='Output dir for file')
 parser.add_argument('count_incr', type=int, default=10, help='Log out every x tables. Choose an integer to use as a divisor', )
 args = parser.parse_args()
 project = args.project
-csv_path = args.csv_path
+file_path = args.file_path
 count_incr = args.count_incr
+operation_mode = args.operation_mode
 
 # create bigquery connection obj
 client = bigquery.Client(project=project)
@@ -28,12 +30,16 @@ def get_table_details(dataset_tablename):
     for i in table_schema:
         type_list.append(i.field_type)
     schema_type_count = dict(Counter(type_list))
-    
     column_list = list()
     for i in table_schema:
         column_list.append(i.name)
-    column_list.sort()
-    
+    #column_list.sort()
+    nullable_list = list()
+    for i in table_schema:
+        nullable_list.append(i.is_nullable)
+    subfields_list = list()
+    for i in table_schema:
+        subfields_list.append(i.fields)
     table_doc['table_name'] = dataset_tablename
     table_doc['full_table_id'] = table.full_table_id
     table_doc['friendly_name'] = table.friendly_name
@@ -60,7 +66,9 @@ def get_table_details(dataset_tablename):
     table_doc['time_partitioning'] = table.time_partitioning
     table_doc['schema_type_count'] = schema_type_count
     table_doc['column_names'] = column_list
-    
+    table_doc['column_type_list'] = type_list
+    table_doc['nullable_list'] = nullable_list
+    table_doc['subfields_list'] = subfields_list
     return table_doc
 
 
@@ -68,43 +76,103 @@ def crawler(project):
     """
     Crawl all of the datasets and tables in the project
     """
-    
     client.list_datasets(project=project)
     datasets = list(client.list_datasets())
     all_tables = []
     counter = 0
     for dataset in datasets:
         dataset_id = dataset.dataset_id
-        tables_list = list(client.list_tables(dataset_id)) 
+        tables_list = list(client.list_tables(dataset_id))
         for table in tables_list:
             dataset_table_name = dataset.dataset_id + '.' + table.table_id
             all_tables.append(dataset_table_name)
-        
             counter += 1
             if counter % count_incr == 0:
                 print(counter, 'tables crawled')
     print(counter, 'tables crawled')
-        
-        
-    
     all_table_details = []
     for table in all_tables:
         table_details = get_table_details(table)
         all_table_details.append(table_details)
-        
     return all_table_details
 
 
 def write_to_csv(all_table_details):
-    
     keys = all_table_details[0].keys()
-    with open(csv_path, 'w') as output_file:
+    with open(file_path, 'w') as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(all_table_details)
-        
-    print('CSV saved to:', csv_path)
-    
+    print('CSV saved to:', file_path)
+
+
+def write_ddl(all_table_details):
+    """
+    Generate CREATE TABLE DDL
+    @todo:
+    Support for ARRAY data type
+    Support for STRUCT more than 1 level
+    Support for PARTITION BY TIMESTAMP and INGESTION TIME
+    Support for PARTITION BY INTEGER RANGE
+    Support for OPTIONS
+    Support for AS
+    Support for VIEWS
+    """
+    #TOKENS
+    create_table_token_open = "CREATE OR REPLACE TABLE "
+    values_token_open = " ("
+    values_separator = " , "
+    create_table_token_close = ")"
+    blank_space = " "
+    new_line = "\n"
+    integer_token_read = "INTEGER"
+    integer_token_write = "INT64"
+    float_token_read = "FLOAT"
+    float_token_write = "FLOAT64"
+    record_token = "RECORD"
+    struct_token_open = "STRUCT< "
+    struct_token_close = " >"
+    not_nullable_token = " NOT NULL"
+    partition_by_token = " PARTITION BY "
+    with open(file_path, 'w') as output_file:
+        for table in all_table_details:
+            output_file.write(create_table_token_open)
+            output_file.write(table['table_name'])
+            output_file.write(values_token_open)
+            counter = 0
+            for column in table['column_names']:
+                output_file.write(column)
+                output_file.write(blank_space)
+                if (table['column_type_list'][counter] == integer_token_read):
+                    output_file.write(integer_token_write)
+                elif (table['column_type_list'][counter] == float_token_read):
+                    output_file.write(float_token_write)
+                elif (table['column_type_list'][counter] == record_token):
+                    output_file.write(struct_token_open)
+                    counter_2 = 0
+                    for subfield in table['subfields_list'][counter]:
+                        output_file.write(subfield.name)
+                        output_file.write(blank_space)
+                        output_file.write(subfield.field_type)
+                        if(not subfield.is_nullable):
+                            output_file.write(not_nullable_token)
+                        if (( counter_2 +1 ) != len( table['subfields_list'][counter] ) ):
+                            output_file.write(values_separator)
+                        counter_2 += 1
+                    output_file.write(struct_token_close)
+                else:
+                    output_file.write(table['column_type_list'][counter])
+                if (not table['nullable_list'][counter]):
+                    output_file.write(not_nullable_token)
+                if (( counter +1 ) != len( table['column_type_list'] ) ):
+                    output_file.write(values_separator)
+                counter += 1
+            output_file.write(create_table_token_close)
+            if (table['time_partitioning'] is not None and table['time_partitioning'].field is not None):
+                output_file.write(partition_by_token)
+                output_file.write(table['time_partitioning'].field)
+            output_file.write(new_line)
+    print('File saved to:', file_path)
 
 
 def main():
@@ -112,8 +180,12 @@ def main():
     all_table_details = crawler(project)
     result_count = len(all_table_details)
     print(result_count, ' tables found')
-    write_to_csv(all_table_details)
+    if(operation_mode == "csv"):
+        write_to_csv(all_table_details)
+    if(operation_mode == "ddl"):
+        write_ddl(all_table_details)
     print('Crawl completed')
+
 
 if __name__ == '__main__':
     main()
